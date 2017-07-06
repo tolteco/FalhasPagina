2f7eb9f02ed1b5ff33ff40d86de700c7ced2a04b
Maycon de Queiroz Oliveira <mscrat@hotmail.com>
2017-07-06 10:55:31 -0300

    Inicio Arquivos
    Adaptações depois...

---
+++ LRUPilha.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * LRU Pilha para substituicao de paginas
+ */
+ 
+//Declaracao de variaveis globais
+FILE *IN;
+
+int main(int argc, char *argv[]){
+	
+	if (argc < 3){
+		printf ("Argumentos Insuficientes\n");
+		return 1;
+	}
+	
+	if ((strcmp(op[1], "-h") == 0) || (strcmp(op[1], "/h") == 0) || (strcmp(op[1], "/help") == 0) || (strcmp(op[1], "-help") == 0)){ //Opcao Help
+      printf("Sistema de arquivos Cumulo V%d.%d\n\n Chamada:\n\n", SVER, MVER);
+      printf("	NOME.x [unidade]\n");
+      printf("  [unidade]    Unidade separada para a unidade (sda, sdb1, ...)\n");
+      return 0;
+    }
+	
+	if (strcmp(argv[1], "-f") == 0 || strcmp(argv[1], "/f") == 0 || strcmp(argv[1], "-F") == 0 || strcmp(argv[1], "/F") == 0){ //Opcao Frames
+		//Opcao de frames
+	}
+	
+	IN = fopen(argv[2], "r");
+	
+	if (IN == NULL){
+      printf("Erro, Arquivo invalido");
+      return 1;
+    }
+
+    return 0;
+}
