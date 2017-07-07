543c757865c49496effb189770c0c09d50f78066
Maycon de Queiroz Oliveira <mscrat@hotmail.com>
2017-07-06 22:24:57 -0300

    Continuacao Inicializacao

--- Otimo.c
+++ Otimo.c
@@ -6,34 +6,34 @@
 /**
  * Algoritmo otimo para substituicao de paginas
  */
- 
+
 //Declaracao de variaveis globais
 FILE *IN;
+int FRAMES;
+int PAGINAS = 0; //A definir
+int DESLOCAMENTO = 0;//A definir
 
 int main(int argc, char *argv[]){
-	
+
 	if (argc < 3){
 		printf ("Argumentos Insuficientes\n");
 		return 1;
 	}
-	
-	if ((strcmp(op[1], "-h") == 0) || (strcmp(op[1], "/h") == 0) || (strcmp(op[1], "/help") == 0) || (strcmp(op[1], "-help") == 0)){ //Opcao Help
-      printf("Sistema de arquivos Cumulo V%d.%d\n\n Chamada:\n\n", SVER, MVER);
-      printf("	NOME.x [unidade]\n");
-      printf("  [unidade]    Unidade separada para a unidade (sda, sdb1, ...)\n");
-      return 0;
-    }
-	
+
 	if (strcmp(argv[1], "-f") == 0 || strcmp(argv[1], "/f") == 0 || strcmp(argv[1], "-F") == 0 || strcmp(argv[1], "/F") == 0){ //Opcao Frames
-		//Opcao de frames
+    FRAMES = atoi(argv[3]); //Converte o tamanho solicitado da unidade para inteiro
+    if (FRAMES <= 0){
+      printf("Quantidade invalida. Deve ser maior que zero\n");
+      return 1;
+    }
 	}
-	
+
 	IN = fopen(argv[2], "r");
-	
+
 	if (IN == NULL){
       printf("Erro, Arquivo invalido");
       return 1;
-    }
+  }
 
-    return 0;
+  return 0;
 }
